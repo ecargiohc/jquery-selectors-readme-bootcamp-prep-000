4776b96042dbb39676a839088e693583f1c679cf
gc6123 <gc6123@bard.edu>
2018-10-22 17:45:19 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,17 @@
-// declare your functions here...
+$(document).ready(paragraphSelector() 
+{
+  var val = $('p').html();
+  alert(val);
+})
+$(document).ready(lastImageSelector() {
+  $('img:last');
+})
+$(document).ready(ninjaBabySelector() {
+  $('#baby-ninja');
+})
+$(document).ready(divSelector() {
+  $('.pics');
+})
+$(document).ready(firstListItem() {
+  $('div ul:first-child');
+})
